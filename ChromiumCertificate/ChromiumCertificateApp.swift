10794a7aaf3ce6db9fab28d16fd914ffd3234dbf
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-07-14 11:51:06 +1000

    Initial Commit

---
+++ ChromiumCertificate/ChromiumCertificateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromiumCertificateApp.swift
+//  ChromiumCertificate
+//
+//  Created by Astrian Zheng on 14/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChromiumCertificateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
